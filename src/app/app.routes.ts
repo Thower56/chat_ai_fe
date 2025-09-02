10d163bfce39ff90e3647be514cb4f498e0ad1e9
Thower56 <charles650@msn.com>
2025-09-01 21:05:21 -0400

    change some config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: ChatComponent },
-  // Add more routes here as needed
+  { path: 'chat', component: ChatComponent },
+  { path: '', redirectTo: 'chat', pathMatch: 'full' }
 ];
